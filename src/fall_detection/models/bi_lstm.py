56bbbd7b55a5b401110735f188391c3383197ccc
TrinhDuongHoan <trinhduonghoan2004@gmail.com>
2026-04-06 17:12:26 +0700

    Modify BiLSTM

--- src/fall_detection/models/bi_lstm.py
+++ src/fall_detection/models/bi_lstm.py
@@ -1,42 +1,30 @@
 import torch
 import torch.nn as nn
 
-class FallBiLSTM(nn.Module):
-    def __init__(self, seq_len=30, feature_dim=66, num_classes=3, hidden_dim1=64, hidden_dim2=128):
-        super().__init__()
-        
-        self.lstm1 = nn.LSTM(
-            input_size=feature_dim, 
-            hidden_size=hidden_dim1, 
-            num_layers=1, 
-            batch_first=True,
-            bidirectional=True
+class BiLSTMModel(nn.Module):
+    def __init__(self, input_dim, hidden_dim=128, num_layers=3, num_classes=3, dropout_prob=0.3):
+        super(BiLSTMModel, self).__init__()
+        self.lstm = nn.LSTM(
+            input_size=input_dim, 
+            hidden_size=hidden_dim, 
+            num_layers=num_layers, 
+            batch_first=True, 
+            dropout=dropout_prob if num_layers > 1 else 0,
+            bidirectional=True 
         )
         
-        self.lstm2 = nn.LSTM(
-            input_size=hidden_dim1 * 2, 
-            hidden_size=hidden_dim2, 
-            num_layers=1, 
-            batch_first=True,
-            bidirectional=True
-        )
+        self.fc1 = nn.Linear(hidden_dim * 2, 128) 
+        self.dropout = nn.Dropout(dropout_prob)
+        self.fc2 = nn.Linear(128, num_classes)
+        self.relu = nn.ReLU()
+
+    def forward(self, x):
+        lstm_out, (h_n, c_n) = self.lstm(x)
+        out = torch.cat((h_n[-2,:,:], h_n[-1,:,:]), dim=1)
+        x = self.dropout(out)
+        x = self.fc1(x)
+        x = self.relu(x)
+        x = self.dropout(x)
+        x = self.fc2(x)
         
-        self.classifier = nn.Sequential(
-            nn.Linear(hidden_dim2 * 2, 64),
-            nn.ReLU(),
-            nn.Dropout(0.3),
-            nn.Linear(64, num_classes)
-        )
-        
-    def forward(self, x): 
-        x, _ = self.lstm1(x)
-        
-        _, (h_n, _) = self.lstm2(x)
-        
-        hidden_forward = h_n[0] 
-        hidden_backward = h_n[1] 
-        
-        final_hidden = torch.cat((hidden_forward, hidden_backward), dim=1) 
-        
-        logits = self.classifier(final_hidden)
-        return logits
+        return x
